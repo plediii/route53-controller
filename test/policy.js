45ee07924b45f2f9e6b662e1495531ab01353f1e
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2015-08-30 17:46:08 +0000

    Refactor route53 test, and add s3 policy tests.

--- test/policy.js
+++ test/policy.js
@@ -2,26 +2,66 @@
 
 var test = require('tape');
 var route53PolicyStatements = require('../lib/route53PolicyStatements');
+var s3PolicyStatements = require('../lib/s3PolicyStatements');
 
 test('route53 statements', function (t) {
-
     t.test('HostedZone', function (s) {
-        s.plan(6);
+        s.plan(2);
         var testZone = 'XXX';
         var statements = route53PolicyStatements({
             HostedZone: testZone
         });
-        s.equal(1, statements.length);
-        var statement = statements[0];
-        var effect = statement.Effect;
-        s.equal('Allow', effect);
-        var actions = statement.Action;
-        s.equal(1, actions.length);
-        var action = actions[0];
-        s.equal("route53:ChangeResourceRecordSets", action);
-        var resources = statement.Resource;
-        s.equal(1, resources.length);
-        var resource = resources[0];
-        s.equal('arn:aws:route53:::hostedzone/' + testZone, resource);
+        s.deepEqual([
+            {
+                "Effect": "Allow",
+                "Action": [
+                    "route53:ChangeResourceRecordSets"
+                ],
+                "Resource": [ 'arn:aws:route53:::hostedzone/' + testZone]
+            }
+        ], statements);
+        s.throws(function () {
+            route53PolicyStatements({});
+        }, /HostedZone/);
+    });
+});
+
+test('s3 statements', function (t) {
+    t.test('Bucket', function (s) {
+        s.plan(2);
+        s.deepEqual([
+            {
+                "Effect": "Allow",
+                "Action": [
+                    "s3:Get*"
+                ],
+                "Resource": [
+                    "arn:aws:s3:::testBucket/testKey"
+                ]
+            }
+        ], s3PolicyStatements({ Bucket: 'testBucket', Key: 'testKey' }));
+
+        s.throws(function () {
+            s3PolicyStatements({ Key: 'key' });
+        }, /Bucket/);
+    });
+
+    t.test('Key', function (s) {
+        s.plan(2);
+        s.deepEqual([
+            {
+                "Effect": "Allow",
+                "Action": [
+                    "s3:Get*"
+                ],
+                "Resource": [
+                    "arn:aws:s3:::testBucket/testKey"
+                ]
+            }
+        ], s3PolicyStatements({ Bucket: 'testBucket', Key: 'testKey' }));
+
+        s.throws(function () {
+            s3PolicyStatements({ Bucket: 'bucket' });
+        }, /Key/);
     });
 });
