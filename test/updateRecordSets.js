13423b538098edd67550cdefefb350f8634b6e47
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2015-09-01 05:18:19 +0000

    Complete updateRecordSets templates tests

--- test/updateRecordSets.js
+++ test/updateRecordSets.js
@@ -5,7 +5,52 @@ var m = require('../lib/updateRecordSets');
 
 test('updateRecordSets', function (t) {
     t.test('recordSetTemplate', function (s) {
-        s.plan(1);
+        s.plan(6);
+        s.deepEqual({
+            "Name": "bar.example.com",
+            "Type": "A",
+            "ResourceRecords": [{
+                Value: '127.1.1.1'
+            }]
+        }, m.recordSetTemplate({
+            "Name": "bar.example.com",
+            "Type": "A",
+        }, ['127.1.1.1']),
+                    "Fills in single IP.");
+        s.deepEqual({
+            "Name": "bar.example.com",
+            "Type": "A",
+            "ResourceRecords": [{
+                Value: '127.1.1.1'
+            }, {
+                Value: '127.1.1.2'
+            }]
+        }, m.recordSetTemplate({
+            "Name": "bar.example.com",
+            "Type": "A",
+        }, ['127.1.1.1', '127.1.1.2']),
+                    "Fills in two ips IP.");
+        s.deepEqual({
+            "Name": "bar.example.com",
+            "Type": "AA",
+            "ResourceRecords": [{
+                Value: '127.1.1.1'
+            }]
+        }, m.recordSetTemplate({
+            "Name": "bar.example.com",
+            "Type": "AA",
+        }, ['127.1.1.1']),
+                    "Type overridable.");
+        s.deepEqual({
+            "Name": "bar.example.com",
+            "Type": "A",
+            "ResourceRecords": [{
+                Value: '127.1.1.1'
+            }]
+        }, m.recordSetTemplate({
+            "Name": "bar.example.com",
+        }, ['127.1.1.1']),
+                    "Type optional.");
         s.deepEqual({
             "Name": "bar.example.com",
             "Type": "A",
@@ -15,9 +60,21 @@ test('updateRecordSets', function (t) {
             }]
         }, m.recordSetTemplate({
             "Name": "bar.example.com",
-            "Type": "A",
             "TTL": 30
         }, ['127.1.1.1']),
-                    "Fills in single IP.");
+                    "Additional parameters available.");
+        s.deepEqual({
+            "Name": "bar.example.com",
+            "Type": "A",
+            "ResourceRecords": [{
+                Value: '127.1.1.1'
+            }]
+        }, m.recordSetTemplate({
+            "Name": "bar.example.com",
+            "ResourceRecords": [{
+                Value: '192.168.0.1'
+            }]
+        }, ['127.1.1.1']),
+                    "ResourceRecords overriden.");
     });
 });
