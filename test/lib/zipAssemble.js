8b21d91b64f841eb965434f670e0ac11175d1a32
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2015-10-28 16:30:13 +0000

    Flesh out zipAssembly tests.

--- test/lib/zipAssemble.js
+++ test/lib/zipAssemble.js
@@ -1,13 +1,136 @@
 "use strict";
 
 var test = require('tape');
+var fs = require('fs');
 var m = require('../../lib/zipAssemble');
 var _ = require('lodash');
 var pathlib = require('path');
 
 test('zipAssemble', function (t) {
 
-    t.test('Adds data file to zip', function (s) {
+    var mockZip = function (params) {
+        var nop = function () {};
+        return {
+            file: params.file ||  nop
+            , folder: params.folder || nop
+            , generate: params.generate || nop
+        };
+    };
+
+    var testPath = pathlib.join(__dirname, '..', 'data');
+
+    t.test('Zips a file', function (s) {
+        s.plan(1);
+        m(mockZip({
+            file: function () {
+                s.pass('called zip.file');
+            }
+        }), testPath, [{ file: 'resource.json' }]);
     });
+
+
+    t.test('Zips multiple files', function (s) {
+        s.plan(3);
+        m(mockZip({
+            file: function () {
+                s.pass('called zip.file');
+            }
+        }), testPath, [
+            { file: 'resource.json' }
+            , { file: 's3location.json' }
+            , { file: 'subdir/test.txt' }
+        ]);
+    });
+
+    t.test('Places file in root path', function (s) {
+        s.plan(1);
+        m(mockZip({
+            file: function (path) { 
+                s.equal(path, 'resource.json');
+            }
+        }), testPath, [{ file: 'resource.json' }]);
+    });
+
+    t.test('Places subdir file in appropriate path', function (s) {
+        s.plan(1);
+        m(mockZip({
+            file: function (path) { 
+                s.equal(path, 'subdir/test.txt');
+            }
+        }), testPath, [{ file: 'subdir/test.txt' }]);
+    });
+
+    t.test('Zips a folder recursively', function (s) {
+        s.plan(4);
+        m(mockZip({
+            file: function (path) {
+                if (path === 'subdir/test.txt') {
+                    s.pass('found folder file');
+                }
+                if (path === 'subdir/subsubdir/subtest.txt') {
+                    s.pass('found folder file');
+                }
+            }
+            , folder: function (path) {
+                if (path === 'subdir') {
+                    s.pass('added subdir');
+                }
+                if (path === 'subdir/subsubdir') {
+                    s.pass('added subsubdir');
+                }
+            }
+        }), testPath, [{ folder: 'subdir' }]);
+    });
+
+    t.test('Places file with expected contents', function (s) {
+        s.plan(1);
+        m(mockZip({
+            file: function (path, data) { 
+                s.equal(fs.readFileSync(testPath + '/resource.json'), data);
+            }
+        }), testPath, [{ file: 'resource.json' }]);
+    });
+
+    t.test('Throws exception if file does not exist', function (s) {
+        s.plan(1);
+        s.throws(function () {
+            m(mockZip({}), testPath, [{ file: 'notexist.json' }]);
+        });
+    });
+
+    t.test('Generates zip after adding file', function (s) {
+        s.plan(1);
+        var fileAdded = false;
+        m(mockZip({
+            file: function (path, data) { 
+                fileAdded = true;
+            }
+            , generate: function () {
+                s.ok(fileAdded);
+            }
+        }), testPath, [{ file: 'resource.json' }]);
+    });
+
+    t.test('Generates nodebuffer', function (s) {
+        s.plan(1);
+        m(mockZip({
+            generate: function (param) {
+                s.equal(param.type, 'nodebuffer');
+            }
+        }), testPath, [{ file: 'resource.json' }]);
+    });
+
+    t.test('Returns the generated result', function (s) {
+        s.plan(1);
+        var result = m(mockZip({
+            generate: function (param) {
+                return 'result';
+            }
+        }), testPath, [{ file: 'resource.json' }]);
+        s.equal(result, 'result');
+    });
+
+
+    
 });
 
