30aeb9cf1ee1f99949bd1e5a8d074839ea67ba83
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2015-11-11 18:00:25 +0000

    Stub out createLambdaPackage tests.

---
+++ test/bin/createLambdaPackage.js
@@ -0,0 +1,63 @@
+"use strict";
+
+var test = require('tape');
+var m = require('../../bin/createLambda');
+var _ = require('lodash');
+var pathlib = require('path');
+var fs = require('fs');
+
+var testResourceFile = pathlib.join(__dirname, '/../data/resource.json');
+var testS3LocationFile = pathlib.join(__dirname, '/../data/s3location.json');
+var testResource = JSON.parse(fs.readFileSync(testResourceFile));
+
+var mockAWS = function (mockParams) {
+    var nop = function () {};
+    return {
+        IAM: function () {
+            return {
+                createPolicy: mockParams.createPolicy || nop
+                , putUserPolicy: mockParams.putUserPolicy || nop
+                , putRolePolicy: mockParams.putRolePolicy || nop
+            };
+        }
+        , S3: function () {
+            return {
+                getObject: function (location, cb) {
+                    if (mockParams && mockParams.getObject) {
+                        return mockParams.getObject(location, cb);
+                    } else {
+                        return cb(null, {
+                            Body: JSON.stringify(testResource)
+                        });
+                    }
+                }
+            };
+        }
+    };
+};
+
+var mockZip = function (params) {
+    var nop = function () {};
+    return {
+        file: params.file ||  nop
+        , folder: params.folder || nop
+        , generate: params.generate || nop
+    };
+};
+
+
+test('createLambda', function (t) {
+    t.test('Creates a zip, and returns the result', function (s) {
+        s.plan(1);
+        m(mockAWS(), mockZip({
+            generate: function (param) {
+                return 'result';
+            }
+        }), [])
+        .then(function (result) {
+            s.equal(result, 'result');
+        });
+    });
+});
+
+
