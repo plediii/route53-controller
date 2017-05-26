a6845964ea779530b439f5bf25ed9556c945eb5e
Paul Ledbetter <ledbetter.paul+github@gmail.com>
2017-05-26 21:20:04 +0000

    Clarify error message when missing arguments.

--- bin/lambdaFunction.js
+++ bin/lambdaFunction.js
@@ -66,7 +66,7 @@ var run = module.exports =  Promise.method(function (aws, zip, args) {
     var s3location = argv.s3location;
     var functionName = argv.name || 'route53-controller';
     if (!(resource || s3location)) {
-        throw new Error('--resource or --s3location is required to create a new lambda function.');
+        throw new Error('--resource or --s3location is required to create a lambda function deployment package.');
     }
     var roleARN = argv.role;
     var zipFile = zipDeployment(zip, {
